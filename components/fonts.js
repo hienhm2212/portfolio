fe8f5daa3baac1d7790378c1bad86883d8432f47
hienhm <blackcat22121996@gmail.com>
2022-08-06 23:31:50 +0700

    Enhance font

--- components/fonts.js
+++ components/fonts.js
@@ -1,13 +1,6 @@
-import { Global, css } from "@emotion/react"
-
-const Fonts = () => {
-  <Global 
-    styles={`
-      @import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap');
-`   }
-  >
-  </Global>
-  
-}
-
+const Fonts = () => (
+  <style jsx global>{`
+    @import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap');
+  `}</style>
+)
 export default Fonts
